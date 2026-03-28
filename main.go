45350cf22e82e2ae36b309348518870c6ae0569d
MichaelJamesL <jamesliman7@gmail.com>
2026-03-28 22:39:02 +0700

    feat: add choices to entry point

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "path/filepath"
+import "fmt"
+
+// import "path/filepath"
 
 func main() {
-	voxelization(filepath.Join("28-pumpkin", "pumpkin", "pumpkin_.obj"))
+	fmt.Println("Choose Feature: ")
+	fmt.Println("1. OBJ Viewer")
+	fmt.Println("2. Voxelization")
+	var choice int
+	fmt.Scanln(&choice)
+	switch choice {
+	case 1:
+		ObjViewer()
+	case 2:
+		fmt.Println("Source Obj Filepath:")
+		var filepath string
+		fmt.Scanln(&filepath)
+		voxelization(filepath)
+	}
 }
